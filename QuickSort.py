dfc1badaf04ed1c90a87086e87fd3141a65c286b
kennycopas <kcopas2@hawkmail.hccfl.edu>
2025-01-08 13:54:58 -0500

    Update QuickSort.py

--- QuickSort.py
+++ QuickSort.py
@@ -2,40 +2,74 @@ class QuickSort:
     def __init__(self, array):
         self.array = array
         self.size = len(array)
-        self.iSort()
+        self.array = self.rSort(array)
+
+    # Simple Median of Three helper function, takes left and right indicies of an array that represents
+    # a subarray and returns the median of the first, middle, and last elements
+    def mot(self, l, r, array=None):
+        if array is None:
+            array = self.array
+        m = int((r - l) / 2 + l)
+        ll, mm, rr = array[l], array[m], array[r]
+        if ll < mm < rr or ll > mm > rr:
+            return m
+        elif mm < ll < rr or mm > ll > rr:
+            return l
+        else:
+            return r
+
+    # Recursive QuickSort method
+    def rSort(self, array):
+        # Base Case
+        if len(array) <= 2:
+            if len(array) > 1 and array[0] > array[1]:
+                array[0], array[1] = array[1], array[0]
+            return array
+        # Set the left and right bounds to the ends of the subarray and select the pivot via mot
+        l, r = 0, len(array) - 1
+        median = self.mot(l, r, array)
+        # Swap the selected pivot with the last element in the subarray and store the pivot value and index
+        array[median], array[r] = array[r], array[median]
+        pivot, pIndex = array[r], r
+        # While the bounds have not crossed, move the bounds toward each other,
+        # swapping elements when both conditions are met
+        while l <= r:
+            # While the left bound is in bounds, has not crossed the right bound, and it's element is
+            # less than the pivot value, move the left bound right
+            while l < len(array) - 1 and l <= r and array[l] < pivot:
+                l += 1
+            # While the right bound is in bounds, has not crossed the left bound, and it's element is
+            # greater than or equal to the pivot value, move the right bound left
+            while r > 0 and l <= r and array[r] >= pivot:
+                r -= 1
+            if l <= r:
+                array[l], array[r] = array[r], array[l]
+        # Once bounds have crossed, swap the pivot with the element at the left bound and
+        # return the sum of QuickSort calls to the two subarrays to the left and right of the pivot,
+        # including the pivot in the middle
+        array[pIndex], array[l] = array[l], array[pIndex]
+        return self.rSort(array[:l]) + [pivot] + self.rSort(array[l + 1:])
 
     # Iterative QuickSort method
     def iSort(self):
-        # Simple Median of Three helper function, takes left and right indicies of an array that represents
-        # a subarray and returns the median of the first, middle, and last elements
-        def mot(l, r):
-            m = int((r - l) / 2 + l)
-            ll, mm, rr = self.array[l], self.array[m], self.array[r]
-            if ll < mm < rr or ll > mm > rr:
-                return m
-            elif mm < ll < rr or mm > ll > rr:
-                return l
-            else:
-                return r
-
         # This queue holds the left and right indicies of the array representing the subarrays that are
         # pushed to the queue during a BFS, initialized with the left and right bounds of the original array
         boundsQueue = [[0, self.size-1]]
         while boundsQueue:
             # Set the left and right bounds to the next coordinates in the queue
             l, r = boundsQueue[0]
-            # Base case
+            # Base Case
             if (r - l) < 2:
                 if self.array[l] > self.array[r]:
                     self.array[l], self.array[r] = self.array[r], self.array[l]
                 boundsQueue.pop(0)
                 continue
             # Find the median of the subarray
-            median = mot(l, r)
+            median = self.mot(l, r)
             # Swap the selected pivot with the last element in the subarray and store the pivot value and index
             self.array[median], self.array[r] = self.array[r], self.array[median]
             pivot, pIndex = self.array[r], r
-            # While bounds are not crossed, move the left and right pivots towards each other,
+            # While the bounds have not crossed, move the bounds toward each other,
             # swapping elements when both conditions are met
             while l <= r:
                 # While the left bound is in bounds, has not crossed the right bound, and is less than
@@ -49,7 +83,7 @@ class QuickSort:
                 # If bounds have not crossed, swap the elements at each bound
                 if l < r:
                     self.array[l], self.array[r] = self.array[r], self.array[l]
-            # Once bounds have crossed, swap the pivot with the element at the right bound,
+            # Once bounds have crossed, swap the pivot with the element at the left bound,
             # push the coordinates of the surrounding subarrays to the queue, and remove the current
             # coordinates from the queue
             self.array[pIndex], self.array[l] = self.array[l], self.array[pIndex]
